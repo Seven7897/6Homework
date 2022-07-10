e55d30552fc12537a2f435b85b4f7282e6959dd7
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-10 21:02:56 +0300

    *FirstCommit*

---
+++ Homework001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите количество чисел");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < number; i++)
+{
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber > 0)
+    {
+        count += + 1; 
+    }
+}
+Console.WriteLine( " количество положительных чисел : " + count );
