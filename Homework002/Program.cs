5243765ab8f6b491fde397f43402900d6b973573
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-16 12:47:52 +0300

    Homework002

---
+++ Homework002/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("b1");
+double b1 = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("k1");
+double k1 = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("b2");
+double b2 = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("k2");
+double k2 = Int32.Parse(Console.ReadLine());
+double x = (-b2 + b1 ) / (-k1 + k2);
+double y = k2 * x + b2 ; 
+Console.WriteLine($"прямые пересекутся в точке X : {x} , Y : {y}");
